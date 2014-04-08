f3f9a436858b9096a98b4ece524754cd119a1fdd
Leo Caseiro <leobok@gmail.com>
2014-04-09 02:59:22 +1000

    Create Form2Helper.php

---
+++ app/View/Helper/Form2Helper.php
@@ -0,0 +1,35 @@
+<?php  
+App::uses('FormHelper', 'View/Helper');
+class Form2Helper extends FormHelper 
+{
+    function input($fieldName, $options = array()) {
+        $this->setEntity($fieldName); 
+         
+        $modelKey = $this->model(); 
+        $fieldKey = $this->field(); 
+
+
+        if (!isset($this->fieldset[$modelKey]['fields'][$fieldKey]))    
+            $this->_introspectModel($modelKey, 'fields', $fieldKey);
+        
+
+        if ((!isset($options['type']) || $options['type'] == 'select' || $options['type'] == 'radio') && !isset($options['options'])) { 
+            if(isset($this->fieldset[$modelKey]['fields'][$fieldKey]))
+            { 
+                $type = $this->fieldset[$modelKey]['fields'][$fieldKey]['type']; 
+                if(substr($type, 0, 4) == 'enum') 
+                { 
+                    $arr = explode('\'', $type); 
+                    $enumValues = array(); 
+                    foreach($arr as $value) 
+                    { 
+                        if($value != 'enum(' && $value != ',' && $value != ')') 
+                            $options['options'][$value] = __($value, true); 
+                    } 
+                } 
+            } 
+        } 
+         
+        return parent::input($fieldName, $options); 
+    } 
+}
